388de8f4a094760f94c400948d6d8e095398859e
ondaa <demnodey@gmail.com>
2021-08-19 00:05:09 +0900

    중간하는중 ~

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var header = document.querySelector("header");
-  var container = document.querySelector(".container");
+  var menuSlider = document.querySelector(".menuSlider");
 
   // body 의 scroll에 따라
   document.addEventListener("scroll", function (ev) {
@@ -11,4 +11,45 @@ window.onload = function () {
       header.classList.remove("sticky");
     }
   });
+
+  // gnb animation
+  var gnbItems = document.querySelectorAll(".navLinkGroup > li");
+  var timer = null;
+
+  Array.from(gnbItems).forEach(function (item) {
+    item.addEventListener("mouseenter", function (ev) {
+      removeOn();
+
+      this.classList.add("on");
+
+      // slider 위치 및 넓이 수정
+      var { offsetWidth, offsetLeft } = this.children[0];
+      menuSlider.style.left = `${offsetLeft}px`;
+      menuSlider.style.width = `${offsetWidth}px`;
+
+      // 350ms 가 지나기 저에 다시 enter 되면 타이머 제거
+      clearTimeout(timer);
+
+      if (!header.classList.contains("menuOpen")) {
+        header.classList.add("menuOpen");
+      }
+    });
+  });
+
+  header.addEventListener("mouseleave", function () {
+    if (header.classList.contains("menuOpen")) {
+      // 350ms 후에 실행
+      timer = setTimeout(function () {
+        menuSlider.style.width = "0px";
+        header.classList.remove("menuOpen");
+        removeOn();
+      }, 350);
+    }
+  });
+
+  function removeOn() {
+    Array.from(gnbItems).forEach(function (item) {
+      item.classList.remove("on");
+    });
+  }
 };
